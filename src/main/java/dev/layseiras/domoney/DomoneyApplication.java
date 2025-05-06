35522dd517d68c7256feecf4e5c3b60bf7b3490b
Layseiras <layseirasdev@outlook.com>
2025-05-06 08:45:39 -0300

    Initial commit

---
+++ src/main/java/dev/layseiras/domoney/DomoneyApplication.java
@@ -0,0 +1,13 @@
+package dev.layseiras.domoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomoneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/layseiras/domoney/DomoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.layseiras.domoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
