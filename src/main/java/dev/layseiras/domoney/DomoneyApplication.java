91bd09476eafe559de34cb841b4c3ca82703ab59
Laysa <laysa.developer@gmail.com>
2025-05-11 12:56:21 -0300

    feat: add credentials to application startup

--- src/main/java/dev/layseiras/domoney/DomoneyApplication.java
+++ src/main/java/dev/layseiras/domoney/DomoneyApplication.java
@@ -1,12 +1,19 @@
 package dev.layseiras.domoney;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class DomoneyApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("API_KEY", dotenv.get("API_KEY"));
+		System.setProperty("API_URL", dotenv.get("API_URL"));
+
 		SpringApplication.run(DomoneyApplication.class, args);
 	}
 
