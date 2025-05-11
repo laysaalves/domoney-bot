f3991963ca6a1b61c1509aa7ec1ed0eb801742f2
Laysa <laysa.developer@gmail.com>
2025-05-11 12:58:00 -0300

    feat: create chatbot controller

---
+++ src/main/java/dev/layseiras/domoney/controller/ChatbotController.java
@@ -0,0 +1,42 @@
+package dev.layseiras.domoney.controller;
+
+import dev.layseiras.domoney.dto.ChatbotRequest;
+import dev.layseiras.domoney.service.ChatbotService;
+import dev.layseiras.domoney.service.ChunkReadService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/chatbot")
+public class ChatbotController {
+
+    private final ChatbotService chatbotService;
+    private final ChunkReadService chunkReadService;
+
+    public ChatbotController(ChatbotService chatbotService, ChunkReadService chunkReadService) {
+        this.chatbotService = chatbotService;
+        this.chunkReadService = chunkReadService;
+    }
+
+    @PostMapping("/ask")
+    public Mono<ResponseEntity<String>> ask(@RequestBody ChatbotRequest request) {
+
+        List<String> relevantChunks = chunkReadService.getRelevantChunks(request.userInput());
+
+
+        if (relevantChunks.isEmpty()) {
+            return Mono.just(ResponseEntity.ok("Não foi encontrado nenhum registro correspondente à sua pergunta."));
+        } else {
+            System.out.println(relevantChunks);
+            return chatbotService.getChatbotOutput(request)
+                    .map(ResponseEntity::ok)
+                    .onErrorResume(e -> Mono.just(ResponseEntity.status(500).body("Erro: " + e.getMessage())));
+        }
+    }
+}
